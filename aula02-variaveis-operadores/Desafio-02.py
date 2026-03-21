c3855aa7103211dbaad537f584b05e07e607d82b
enzoselingardecoppa <enzoselingardecoppa@gmail.com>
2026-03-21 20:39:53 -0300

    1CCPY-Python-FIAP-2026

---
+++ aula02-variaveis-operadores/Desafio-02.py
@@ -0,0 +1,4 @@
+Dia = int(input('Digite o dia do seu nascimento: '))
+Mes = int(input('Digite  o mês do seu nascimento: '))
+Ano = int(input('Digite  o ano do seu nascimento: '))
+print(f" vc nasceu no dia {Dia} no mês {Mes}  de {Ano}!")
---
+++ aula02-variaveis-operadores/Desafio-03.py
@@ -0,0 +1,3 @@
+num1 = int(input('Digite o primeiro número: '))
+num2 = int(input('Digite o segundo número: '))
+print(num1 + num2)
