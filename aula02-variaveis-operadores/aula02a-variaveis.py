2c4c6245598d5eeaa517fc519491bec486c2788b
enzoselingardecoppa <enzoselingardecoppa@gmail.com>
2026-03-20 22:32:01 -0300

    aula 2 variáveis e operadores

---
+++ aula02-variaveis-operadores/aula02a-variaveis.py
@@ -0,0 +1,31 @@
+print('ola mundo')
+
+print(7+4)
+
+print('7+4')
+
+print('7'+'4') #Concatenação de strings
+
+'''''
+Comentários de 
+multiplas linhas
+
+'''''
+
+
+
+# variáveis
+nome = "Enzo" #string - texto
+idade = 18 #int - numero
+peso = 78.2 #float - num decimal
+
+print(nome, idade, peso)
+print(f"Oiii, {nome}!!!!")
+
+#INPUTS - SIMULAÇÃO DE FORMS NO CMD
+nome = input('Digite seu nome: ')
+idade = int(input('Digite sua idade: '))
+
+print(f"Oiii {nome} ! vc tem {idade} anos")
+nova_idade = idade + 1
+print(nova_idade)
