c78804359cdb99a525d79995208601d13745a7f1
enzoselingardecoppa <enzoselingardecoppa@gmail.com>
2026-05-08 22:23:31 -0300

    1CCPY-Python-FIAP-2026

---
+++ Aula05-Arrays/aula05a-arrays.py
@@ -0,0 +1,14 @@
+lista_frutas = ["Pêssego", "Maçã", "Morango"]
+
+lista_frutas.append("Banana")
+
+print(lista_frutas[-1])
+print()
+
+for i in range(len(lista_frutas)):
+    print(lista_frutas[i])
+
+print()
+
+for fruta in lista_frutas:
+    print(fruta)
